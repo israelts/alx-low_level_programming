d29599c214da618c95607ccd4d86046d92f7722c
israel tsopgue <tsopgueisrael@gmail.com>
2021-06-17 12:42:37 -0700

    added file to 0x00-hello_world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/**
+ * 4. Hello, puts
+ *first program in C.
+ */ 
+#include <stdio.h>
+/**
+ * main - prints the phrase "Programming is like building a multilingual puzzle
+ *Use the function puts
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
+}/**
+  *end function main
+  */
