f122a3e3a6c5a03930d2eb557df908a3798965cb
israel tsopgue <tsopgueisrael@gmail.com>
2021-06-17 13:57:29 -0700

    adde file to 0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -10,8 +10,6 @@
  */
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
-}/**
-  *end function main
-  */
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
