ac70833ae8ffa9c0a94ed0f4bcfca16728ce4cae
israel tsopgue <tsopgueisrael@gmail.com>
2021-06-23 13:32:57 -0700

    added file to 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/9-main.c
+++ 0x02-functions_nested_loops/9-main.c
@@ -1,23 +1,11 @@
 #include "holberton.h"
 
-
-
 /**
-
  * main - check the code for ALX School students.
-
- *
-
  * Return: Always 0.
-
  */
-
 int main(void)
-  
 {
-  
-  times_table();
-  
-  return (0);
-  
+times_table();
+return (0);
 }
