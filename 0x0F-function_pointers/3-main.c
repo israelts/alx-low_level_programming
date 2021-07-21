fefc5e41c1d89602271fd308929dc4ef2e7c20a6
israel tsopgue <tsopgueisrael@gmail.com>
2021-07-21 15:14:11 -0700

    added file to 0x0F-function_pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
 {
 int one, two, ans;
 int (*res)(int, int);
-char *get_op; 
+char *get_op;
 if (argc != 4)
 {
 printf("Error\n");
@@ -26,7 +26,7 @@ one = atoi(argv[1]);
 two = atoi(argv[3]);
 get_op = argv[2];
 
-  /* added edge case if argv[2] was longer than 1 char*/  
+/* added edge case if argv[2] was longer than 1 char*/
 if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
 {
 printf("Error\n");
